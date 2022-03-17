6a2bf7b6adc7491c261a61d1b490edc918ebd1f9
Eduardo Bresolin <75597388+BresoDEV@users.noreply.github.com>
2022-03-17 20:19:43 -0300

    Update Leitor.cpp

--- Leitor.cpp
+++ Leitor.cpp
@@ -1,3 +1,19 @@
+// d88888b db    db d888888b d88888b d8888b. d8b   db       d8888b. d88888b  .d8b.  d8888b. d88888b d8888b.
+// 88'     `8b  d8' `~~88~~' 88'     88  `8D 888o  88       88  `8D 88'     d8' `8b 88  `8D 88'     88  `8D
+// 88ooooo  `8bd8'     88    88ooooo 88oobY' 88V8o 88       88oobY' 88ooooo 88ooo88 88   88 88ooooo 88oobY'
+// 88~~~~~  .dPYb.     88    88~~~~~ 88`8b   88 V8o88       88`8b   88~~~~~ 88~~~88 88   88 88~~~~~ 88`8b
+// 88.     .8P  Y8.    88    88.     88 `88. 88  V888       88 `88. 88.     88   88 88  .8D 88.     88 `88.
+// Y88888P YP    YP    YP    Y88888P 88   YD VP   V8P       88   YD Y88888P YP   YP Y8888D' Y88888P 88   YD
+
+// d8888b. db    db       d8888b. d8888b. d88888b .d8888.  .d88b.  d8888b. d88888b db    db
+// 88  `8D `8b  d8'       88  `8D 88  `8D 88'     88'  YP .8P  Y8. 88  `8D 88'     88    88
+// 88oooY'  `8bd8'        88oooY' 88oobY' 88ooooo `8bo.   88    88 88   88 88ooooo Y8    8P
+// 88~~~b.    88          88~~~b. 88`8b   88~~~~~   `Y8b. 88    88 88   88 88~~~~~ `8b  d8'
+// 88   8D    88          88   8D 88 `88. 88.     db   8D `8b  d8' 88  .8D 88.      `8bd8'
+// Y8888P'    YP          Y8888P' 88   YD Y88888P `8888Y'  `Y88P'  Y8888D' Y88888P    YP
+
+
+
 #include<iostream>
 #include<stdio.h>
 #include<fstream>
